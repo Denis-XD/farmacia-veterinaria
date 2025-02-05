8141b9238779bd1e73e952aeacc275638206f152
Denis-XD <dxgiovanni15@gmail.com>
2025-02-05 02:14:47 -0400

    terminado al 99.6%

--- resources/views/pdf/compras.blade.php
+++ resources/views/pdf/compras.blade.php
@@ -1,84 +1,89 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compra {{ $compra->id_compra }}</title>
+    <title>Compra #{{ $compra->id_compra }}</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            margin: 20px;
+            font-size: 10px;
+            margin: 5px;
         }
 
         .header {
             display: flex;
             justify-content: space-between;
             align-items: center;
+            border-bottom: 1px solid #000;
+            padding-bottom: 2px;
         }
 
         .logo {
-            width: 100px;
-            height: auto;
-        }
-
-        .header-left {
-            font-size: 14px;
+            width: 60px;
         }
 
         h1,
         h2 {
             text-align: center;
-            margin: 10px 0;
+            margin: 3px 0;
+            font-size: 11px;
+        }
+
+        .info p {
+            margin: 2px 0;
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 20px;
+            margin-top: 3px;
         }
 
-        table th,
-        table td {
+        th,
+        td {
             border: 1px solid #000;
-            padding: 8px;
+            padding: 3px;
             text-align: center;
         }
 
-        table th {
+        th {
             background-color: #f2f2f2;
+            font-size: 10px;
+        }
+
+        td {
+            font-size: 9px;
         }
 
         .totals {
-            margin-top: 10px;
             text-align: right;
             font-weight: bold;
+            margin-top: 3px;
         }
     </style>
 </head>
 
 <body>
     <div class="header">
-        <div>
-            <img src="{{ public_path('assets/logo.jpeg') }}" alt="Logo" class="logo">
-        </div>
-        <div>
-            <h1>Farmacia Veterinaria ALVA</h1>
-        </div>
+        <img src="{{ public_path('assets/logo.jpeg') }}" alt="Logo" class="logo">
+        <h1>Farmacia Veterinaria ALVA</h1>
     </div>
-    <h2>Numero de compra: {{ $compra->id_compra }}</h2>
-    <div class="header-left">
+
+    <h2>Compra #{{ $compra->id_compra }}</h2>
+    <div class="info">
         <p><strong>Proveedor:</strong> {{ $compra->proveedor->nombre_proveedor }}</p>
         <p><strong>Fecha:</strong> {{ $compra->fecha_compra->format('d/m/Y') }}</p>
     </div>
 
-    <h2>Detalles de Productos</h2>
+    <h2>Productos</h2>
     <table>
         <thead>
             <tr>
                 <th>CANT.</th>
                 <th>DESCRIPCIÓN</th>
-                <th>PRECIO UNITARIO</th>
+                <th>PRECIO U.</th>
                 <th>SUBTOTAL</th>
             </tr>
         </thead>
@@ -97,7 +102,7 @@
     </table>
 
     <div class="totals">
-        <p>Total suma: Bs {{ number_format($sumaSubtotales, 2) }}</p>
+        <p>Total Productos: Bs {{ number_format($sumaSubtotales, 2) }}</p>
         <p>Descuento: {{ $compra->descuento_compra }}%</p>
         <p>Total: Bs {{ number_format($compra->total_compra, 2) }}</p>
     </div>
--- resources/views/pdf/ventas.blade.php
+++ resources/views/pdf/ventas.blade.php
@@ -1,87 +1,91 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Venta {{ $venta->id_venta }}</title>
+    <title>Venta #{{ $venta->id_venta }}</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            margin: 20px;
+            font-size: 10px;
+            margin: 5px;
         }
 
         .header {
             display: flex;
             justify-content: space-between;
             align-items: center;
+            border-bottom: 1px solid #000;
+            padding-bottom: 2px;
         }
 
         .logo {
-            width: 100px;
-            height: auto;
-        }
-
-        .header-left {
-            font-size: 14px;
+            width: 60px;
         }
 
         h1,
         h2 {
             text-align: center;
-            margin: 10px 0;
+            margin: 3px 0;
+            font-size: 11px;
+        }
+
+        .info p {
+            margin: 2px 0;
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 20px;
+            margin-top: 3px;
         }
 
-        table th,
-        table td {
+        th,
+        td {
             border: 1px solid #000;
-            padding: 8px;
+            padding: 3px;
             text-align: center;
         }
 
-        table th {
+        th {
             background-color: #f2f2f2;
+            font-size: 10px;
+        }
+
+        td {
+            font-size: 9px;
         }
 
         .totals {
-            margin-top: 10px;
             text-align: right;
             font-weight: bold;
+            margin-top: 3px;
         }
     </style>
 </head>
 
 <body>
     <div class="header">
-        <div>
-            <img src="{{ public_path('assets/logo.jpeg') }}" alt="Logo" class="logo">
-        </div>
-        <div>
-            <h1>Farmacia Veterinaria ALVA</h1>
-        </div>
+        <img src="{{ public_path('assets/logo.jpeg') }}" alt="Logo" class="logo">
+        <h1>Farmacia Veterinaria ALVA</h1>
     </div>
 
-    <h2>Numero de venta: {{ $venta->id_venta }}</h2>
-    <div class="header-left">
+    <h2>Nota de entrega: {{ $venta->id_venta }}</h2>
+    <div class="info">
         <p><strong>Atendido por:</strong> {{ $venta->usuario->nombre }}</p>
         <p><strong>Socio:</strong> {{ $venta->socio->nombre_socio ?? 'Sin Socio' }}</p>
         <p><strong>Fecha:</strong> {{ $venta->fecha_venta->format('d/m/Y') }}</p>
-        <p><strong>Total Venta: Bs</strong> {{ number_format($venta->total_venta, 2) }}</p>
+        <p><strong>Total: Bs</strong> {{ number_format($venta->total_venta, 2) }}</p>
     </div>
 
-    <h2>Detalles de Productos</h2>
+    <h2>Productos</h2>
     <table>
         <thead>
             <tr>
                 <th>CANT.</th>
                 <th>DESCRIPCIÓN</th>
-                <th>PRECIO UNITARIO</th>
+                <th>PRECIO U.</th>
                 <th>SUBTOTAL</th>
             </tr>
         </thead>
@@ -109,7 +113,7 @@
             <tr>
                 <th>FECHA</th>
                 <th>MONTO</th>
-                <th>SALDO PENDIENTE</th>
+                <th>SALDO</th>
             </tr>
         </thead>
         <tbody>
